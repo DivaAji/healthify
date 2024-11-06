8f3364dd6a2244845ddd7dad3947b9509916a31b
Silfi Nazarina <silfinazarina@gmail.com>
2024-11-06 20:51:07 +0700

    add scan face screen

--- lib/screens/faceScan_screen.dart
+++ lib/screens/faceScan_screen.dart
@@ -1,11 +1,109 @@
 import 'package:flutter/material.dart';
+import 'package:healthify/widgets/camera.dart';
 
 class FaceScan extends StatelessWidget {
   const FaceScan({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold();
+    // Mendapatkan ukuran layar menggunakan MediaQuery
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              CameraWidget(),
+              Align(
+                alignment: Alignment.topRight,
+                child: Padding(
+                  padding: EdgeInsets.only(
+                    top: screenHeight *
+                        0.05, // Padding atas 5% dari tinggi layar
+                    right:
+                        screenWidth * 0.08, // Padding kanan 8% dari lebar layar
+                  ),
+                  child: Container(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: screenWidth * 0.03,
+                      vertical: screenHeight * 0.01,
+                    ),
+                    decoration: BoxDecoration(
+                      color: const Color.fromRGBO(214, 222, 222, 1),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          'Unggah',
+                          style: TextStyle(
+                            color: const Color.fromRGBO(0, 139, 144, 1),
+                            fontSize:
+                                screenWidth * 0.04, // Ukuran teks responsif
+                          ),
+                        ),
+                        SizedBox(width: screenWidth * 0.02), // Jarak responsif
+                        Image.asset(
+                          'assets/icons/upload.png',
+                          width: screenWidth * 0.05,
+                          height: screenWidth * 0.05,
+                          fit: BoxFit.cover,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Center(
+                child: Padding(
+                  padding: EdgeInsets.only(top: screenHeight * 0.3),
+                  child: Image.asset(
+                    'assets/images/detect_rectangle.png',
+                    width: screenWidth * 0.8, // Lebar gambar responsif
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: screenHeight * 0.02),
+          Text(
+            'Pastikan wajah anda terlihat jelas',
+            style: TextStyle(
+              fontSize: screenWidth * 0.04,
+              fontWeight: FontWeight.w400,
+              color: Color.fromRGBO(33, 50, 75, 1),
+            ),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: screenHeight * 0.01),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.only(bottom: screenHeight * 0.02),
+              child: GestureDetector(
+                onTap: () {
+                  _takePicture();
+                },
+                child: Image.asset(
+                  'assets/images/ellipse.png',
+                  width: screenWidth * 0.2, // Lebar responsif
+                  height: screenWidth *
+                      0.2, // Tinggi responsif agar tetap proporsional
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  // Fungsi untuk mengambil gambar
+  void _takePicture() {
+    // Implementasikan logika pengambilan gambar menggunakan camera
   }
 }
---
+++ lib/widgets/camera.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+
+class CameraWidget extends StatefulWidget {
+  @override
+  _CameraWidgetState createState() => _CameraWidgetState();
+}
+
+class _CameraWidgetState extends State<CameraWidget> {
+  CameraController? _cameraController;
+  List<CameraDescription>? cameras;
+  int _selectedCameraIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    initCamera();
+  }
+
+  // Inisialisasi kamera dan setel kamera depan jika ada
+  Future<void> initCamera() async {
+    cameras = await availableCameras();
+    if (cameras!.isNotEmpty) {
+      // Cari kamera depan jika tersedia
+      _selectedCameraIndex = cameras!.indexWhere(
+          (camera) => camera.lensDirection == CameraLensDirection.front);
+
+      // Jika tidak ada kamera depan, gunakan kamera pertama
+      if (_selectedCameraIndex == -1) {
+        _selectedCameraIndex = 0;
+      }
+
+      _initializeCamera(_selectedCameraIndex);
+    }
+  }
+
+  // Inisialisasi kamera dengan index yang dipilih
+  Future<void> _initializeCamera(int cameraIndex) async {
+    _cameraController?.dispose(); // Bersihkan kontroler kamera sebelumnya
+    _cameraController = CameraController(
+      cameras![cameraIndex],
+      ResolutionPreset.high,
+    );
+
+    await _cameraController!.initialize();
+    setState(() {});
+  }
+
+  // Fungsi untuk mengganti kamera
+  void _toggleCamera() {
+    _selectedCameraIndex = (_selectedCameraIndex + 1) % cameras!.length;
+    _initializeCamera(_selectedCameraIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_cameraController == null || !_cameraController!.value.isInitialized) {
+      return Center(child: CircularProgressIndicator());
+    }
+
+    bool isFrontCamera = cameras![_selectedCameraIndex].lensDirection ==
+        CameraLensDirection.front;
+
+    return Stack(
+      children: [
+        Transform(
+          alignment: Alignment.center,
+          transform:
+              isFrontCamera ? Matrix4.rotationY(3.14159) : Matrix4.identity(),
+          child: CameraPreview(_cameraController!),
+        ),
+        Positioned(
+          bottom: 20,
+          right: 20,
+          child: FloatingActionButton(
+            onPressed: _toggleCamera,
+            child: Icon(Icons.cameraswitch),
+            backgroundColor: const Color.fromRGBO(214, 222, 222, 1), // Warna latar belakang tombol
+            foregroundColor: const Color.fromRGBO(0, 139, 144, 1), // Warna ikon
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  void dispose() {
+    _cameraController?.dispose();
+    super.dispose();
+  }
+}
