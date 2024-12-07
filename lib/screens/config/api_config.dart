ae4a21b133f6146c1f7f116281dd872fed7d6827
DivaAji <125537575+DivaAji@users.noreply.github.com>
2024-12-07 14:19:59 +0700

    Https

--- lib/screens/config/api_config.dart
+++ lib/screens/config/api_config.dart
@@ -1,5 +1,5 @@
 class ApiConfig {
-  static const String baseUrl = 'http://healthify.web.id/api';
+  static const String baseUrl = 'https://healthify.web.id/api';
 
   static const String userEndpoint = "$baseUrl/user";
   static const String manualAgeEndpoint = "$baseUrl/submit-age-manual";
